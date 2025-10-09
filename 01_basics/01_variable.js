297bf3ad2c6868bc2a4c446f569d96fb1d522f2c
Dinesh Nandane <118107086+Dinesh-Nandane@users.noreply.github.com>
2025-10-09 18:50:19 +0000

    created files

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 3544854908
+let accountEmailId = "dinesh@gmail.com"
+var accountPassword = "fddfkj0930ndf**&*n"
+accountCity = "Bengluru"
+let accountState;
+// accountId = 40940498  are not allowed//
+
+accountEmailId = "dineshna"
+accountPassword = "3093jfoe"
+accountCity = "Amravati"
+
+console.table([accountId,accountEmailId,accountPassword,accountCity,accountState])
+
+
---
+++ 01_basics/test.js
@@ -0,0 +1,2 @@
+console.log("Dinesh")
+console.log("Nandane")
