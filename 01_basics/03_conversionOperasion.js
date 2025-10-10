c914b18d4ff39c910cc4b9a5798c29014b721e87
Dinesh Nandane <118107086+Dinesh-Nandane@users.noreply.github.com>
2025-10-10 19:07:45 +0000

    added nwe file

---
+++ 01_basics/03_conversionOperasion.js
@@ -0,0 +1,42 @@
+// let score = undefined
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// 33 => 33
+// "33" =>33
+// "33dfldfjp" => NaN
+// true =>1; false =>0
+// undefinded => NaN
+
+
+// let isLogging = NaN
+
+// let isBoolean = Boolean(isLogging)
+
+// console.log(typeof (isBoolean))
+// console.log(isBoolean)
+
+// true => true ;false => false
+// 1 => true ; 0 => false
+// "fgfklgfjflkgj" => true 
+// "" => false
+// 4540958 => true
+// null => false
+// undefined => false
+// NaN => flase
+
+
+let Name = undefined
+
+let isString = String(Name)
+
+console.log(typeof isString)
+console.log(isString)
+
+
+
