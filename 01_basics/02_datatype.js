2cf5dec2c193fe0c229d336bd80d00eaabe5228e
Dinesh Nandane <118107086+Dinesh-Nandane@users.noreply.github.com>
2025-10-10 17:51:15 +0000

    added new file

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+// number => 2 power of 53 
+// bigint
+// string => ""
+// boolean => true or false 
+// null => standalone value
+// undefined 
+// symbole =>unique
+// object 
+
+
+let Name = "Dinesh"
+let Age = 27
+
+let undefined_value
+
+console.log(typeof Name)
+console.log(typeof Age)
+console.log(typeof undefined_value)  // undefined
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
